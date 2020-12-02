4c8f2ba86d49dcd12c5884e3ba99184bb0bc8b12
Harrison <yeeharrison57@yahoo.com>
2020-12-02 10:40:22 -0800

    Fixed welcome message

--- Mini_Facebook/part1/server.py
+++ Mini_Facebook/part1/server.py
@@ -60,7 +60,7 @@ def clientThread(conn):
 	global clients
 	global count
 	# Tips: Sending message to connected client
-	conn.send('Welcome to the server. Type something and hit enter\n') #send only takes string
+	conn.send('Welcome to the server. Please enter your username and password.') #send only takes string
 	rcv_msg = conn.recv(1024)
 	rcv_msg = stringToTuple(rcv_msg)
 	if rcv_msg in userpass:
